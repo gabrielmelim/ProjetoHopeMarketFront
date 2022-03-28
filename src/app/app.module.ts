d87508e7d6dbf70a4943046df35600cdb92f07a4
Jamille <lino.jamille@gmail.com>
2022-03-28 18:02:40 -0300

    @jamilleLino [Task: Criação do componente contato]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ContatoComponent } from './contato/contato.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+
+    ContatoComponent,
   ],
   imports: [
     BrowserModule,
