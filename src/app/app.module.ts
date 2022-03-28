3ce3248842e62527c19999eff443b222f757d287
unknown <geoo.vanna2305@gmail.com>
2022-03-28 17:52:44 -0300

    @GiovanaGS [Task: Criação do componente Sobre]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { SobreComponent } from './sobre/sobre.component';
-
 @NgModule({
   declarations: [
     AppComponent,
