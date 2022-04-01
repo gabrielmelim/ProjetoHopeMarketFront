89872433fc630d643eaec53597763ca5b173ae6e
unknown <tamara-augusta@hotmail.com>
2022-04-01 09:28:45 -0300

    @tamara-augusta Task: Criação componente menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { ContatoComponent } from './contato/contato.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RodapeComponent,
+    SobreComponent,
+    ContatoComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
