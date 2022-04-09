092d2747929038c235b9e8e8618d19521ec959bb
unknown <jeferson.aristaque@gmail.com>
2022-04-09 13:16:57 -0300

    @Aristaque [Task: Implementação do editProd]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
   {path: 'cadproduto', component: CadprodutoComponent},
   {path: 'sobre', component: SobreComponent},
   {path: 'home', component: HomeComponent}
-
+  
 ];
 
 @NgModule({
