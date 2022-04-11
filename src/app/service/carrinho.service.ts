42592469fbeb954d69c2728436a0709fd83b0587
unknown <tamara-augusta@hotmail.com>
2022-04-11 16:52:59 -0300

    @tamara-augusta Task[Implementado funcionalidade no carrinho]

--- src/app/service/carrinho.service.ts
+++ src/app/service/carrinho.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Produto } from '../model/Produto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarrinhoService {
 
-  constructor() { }
+  produtos: Produto [] = []
+  totalItems: number
+
+  constructor(
+    private router: Router
+  ) { }
+
+  adicionar(produto: Produto) {
+    this.produtos.push(produto)
+    this.totalItems = this.produtos.length
+  }
+
+  listar() {
+    return this.produtos
+  }
+
+  limpar() {
+    this.produtos = [];
+    return this.produtos
+  }
+
+  confirmar() {
+    this.produtos = []
+    alert('Parabéns! Compra realizada com sucesso.')
+    return this.produtos
+    this.router.navigate(['/produto'])
+
+  }
+
 }
